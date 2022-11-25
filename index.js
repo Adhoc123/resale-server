8cb3ec9d227a1343aed46aec04e634d848f5ce71
Adhoc123 <emonhasan1010@gmail.com>
2022-11-25 20:06:17 +0600

    added structure

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+app.get('/', async(req, res)=>{
+    res.send('resale is running');
+})
+
+app.listen(port, ()=> console.log(`Server is running on ${port}`))
