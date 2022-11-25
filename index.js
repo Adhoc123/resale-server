decf96c60602806563c7de0b14ceb39dd7a7a3b1
Adhoc123 <emonhasan1010@gmail.com>
2022-11-25 22:16:08 +0600

    received data

--- index.js
+++ index.js
@@ -16,28 +16,20 @@ console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
-      const database = client.db("sample_mflix");
-      const movies = database.collection("movies");
-      // query for movies that have a runtime less than 15 minutes
-      const query = { runtime: { $lt: 15 } };
-      const options = {
-        // sort returned documents in ascending order by title (A->Z)
-        sort: { title: 1 },
-        // Include only the `title` and `imdb` fields in each returned document
-        projection: { _id: 0, title: 1, imdb: 1 },
-      };
-      const cursor = movies.find(query, options);
-      // print a message if no documents were found
-      if ((await cursor.count()) === 0) {
-        console.log("No documents found!");
-      }
-      // replace console.dir with your callback to access individual elements
-      await cursor.forEach(console.dir);
-    } finally {
-      await client.close();
+      const categoryCollection = client.db("resale").collection("category");
+      
+      app.get('/category', async(req, res)=>{
+           const query = {};
+           const categories = await categoryCollection.find(query).toArray();
+           res.send(categories);
+      })
+    
+    }
+    finally {
+      
     }
   }
-  run().catch(console.dir);
+run().catch(console.dir);
 
 
 app.get('/', async(req, res)=>{
