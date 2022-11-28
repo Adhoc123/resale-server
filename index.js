a05d583969627f7e33e1a9241638ff3a92e74cbd
Adhoc123 <emonhasan1010@gmail.com>
2022-11-28 16:07:18 +0600

    fetching booking data

--- index.js
+++ index.js
@@ -41,6 +41,12 @@ async function run() {
            const electriccar = await electriccarCollection.find(query).toArray();
            res.send(electriccar);
       })
+      app.get('/bookings', async(req, res)=>{
+          const email = req.body.email;
+          const query = {email:email};
+          const bookings = await bookingsCollection.find(query).toArray();
+          res.send(bookings);
+      })
       app.post('/bookings', async(req, res)=>{
           const booking = req.body;
           const result = await bookingsCollection.insertOne(booking);
